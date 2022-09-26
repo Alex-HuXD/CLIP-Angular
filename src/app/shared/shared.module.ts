3b9384d8409896d9e0d54ecb000a27afab5047d4
XinDi Hu <huxdev@pm.me>
2022-09-25 21:28:06 -0500

    add new components - shared/TabsContainer - shared/Tab

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 // list service in the provider array make this service available to everything inside this module
 // import { ModalService } from '../services/modal.service';
 
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule],
-  exports: [ModalComponent],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent],
 
   // providers:[ModalService]
 })
---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
